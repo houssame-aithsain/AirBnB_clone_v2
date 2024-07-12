4041aceb299d915f2265b3ed06e7ee32b55c260c
Elpira <elpira@elpira.168.1.36>
2024-07-11 21:10:02 -0400

    0x000

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,9 @@
 Module to deploy an archive to web servers using Fabric.
 """
 
-import os.path
-from fabric.api import *
-from fabric.operations import run, put
 from datetime import datetime
+from fabric.api import *
+import os
 
 env.hosts = ['54.84.246.32', '52.91.154.11']
 
@@ -15,43 +14,38 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
 
     local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
 
-    result = local("tar -czvf versions/web_static_{}.tgz web_static"
-                   .format(now))
-    if result.failed:
-        return None
+    if t_gzip_archive.succeeded:
+        return archived_f_path
     else:
-        return result
+        return None
 
 
 def do_deploy(archive_path):
     """
     Distributes an archive to the web servers.
     """
-    if not os.path.exists(archive_path):
-        return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        print("New version deployed!")
+        return True
 
-        print('New version deployed!')
-        success = True
-
-    except Exception:
-        success = False
-        print("Could not deploy")
-    return success
+    return False
