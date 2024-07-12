39ca7a728acf53c6d3542460a54f7f3643b4fa5f
Elpira <elpira@elpira.168.1.36>
2024-07-11 20:49:30 -0400

    0x000

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,23 +19,20 @@ def do_deploy(archive_path):
     Returns:
         bool: True if deployment was successful, False otherwise.
     """
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
-        dest_dir = "/data/web_static/releases/" + no_ext + "/"
-        put(archive_path, "/tmp/" + file_name)
-        run("mkdir -p " + dest_dir)
-        run("tar -xzf /tmp/" + file_name + " -C " + dest_dir)
-        run("rm /tmp/" + file_name)
-        run("mv " + dest_dir + "web_static/* " + dest_dir)
-        run("rm -rf " + dest_dir + "web_static")
-        run("rm -rf /data/web_static/current")
-        run("ln -s " + dest_dir + " /data/web_static/current")
-        print("New version deployed!")
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception as e:
-        print(f"Deployment failed: {e}")
         return False
