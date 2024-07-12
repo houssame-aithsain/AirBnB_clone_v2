8834de21bd87aecb893b7075366fea5077640525
Elpira <elpira@elpira.168.1.36>
2024-07-11 20:59:43 -0400

    0x000

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,37 +3,55 @@
 Module to deploy an archive to web servers using Fabric.
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+import os.path
+from fabric.api import *
+from fabric.operations import run, put
+from datetime import datetime
 
 env.hosts = ['54.84.246.32', '52.91.154.11']
 
 
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    local("mkdir -p versions")
+
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(now))
+    if result.failed:
+        return None
+    else:
+        return result
+
 
 def do_deploy(archive_path):
     """
-    Deploy archive!
-    Returns False if the file at the path archive_path does't exist
+    Distributes an archive to the web servers.
     """
-
-    if !exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
-
-    file_name = archive_path.split('/')[-1]
-    rm_extension = "/data/web_static/releases/" + '{}'\
-        .format(file_name.split('.'))
-    tmp = "/tmp" + file_name
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
 
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(rm_extension))
-        run("tar -xzf {} -C {}/".format(rm_extension))
-        run("rm {}".format(tmp))
-        run("mv -rf {}/web_static/* {}/".format(rm_extension, rm_extension))
-        run("rm -rf {}/web_static".format(rm_extension))
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(rm_extension))
+        run("ln -s {} /data/web_static/current".format(folder_path))
 
-        return True
-    except:
-        return False
+        print('New version deployed!')
+        success = True
+
+    except Exception:
+        success = False
+        print("Could not deploy")
+    return success
