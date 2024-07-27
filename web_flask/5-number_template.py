32d839adf0043445014d0b2cb0e068b2c905813c
Elpira <elpira@elpira.168.1.36>
2024-07-27 13:35:13 -0400

    web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,43 +1,27 @@
 #!/usr/bin/python3
 """
-A simple Flask web application that displays:
-- 'Hello HBNB!' on the root route
-- 'HBNB' on the /hbnb route
-- 'C ' followed by the value of the text variable on the /c/<text> route
-- 'Python ' followed by the value of the text variable on the /python/<text>
-    route
-  (with a default value of 'is cool')
-- 'n is a number' only if n is an integer on the /number/<n> route
-- A HTML page with 'Number: n' inside an H1 tag only if n is an integer on the
-    /number_template/<n> route
+a script that starts a Flask web application
 """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Displays 'Hello HBNB!' on the root route.
-    """
-    return "Hello HBNB!"
+def hello_world():
+    """prints Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Displays 'HBNB' on the /hbnb route.
-    """
-    return "HBNB"
+    """prints HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """
-    Displays 'C ' followed by the value of the text variable.
-    Replaces underscores in the text variable with spaces.
-    """
-    return "C " + text.replace('_', ' ')
+    """prints C followed by text"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
@@ -48,20 +32,16 @@ def python_text(text='is cool'):
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """
-    Displays 'n is a number' only if n is an integer.
-    """
-    return f"{n} is a number"
+def number_n(n):
+    """prints n is a number"""
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """
-    Displays a HTML page with 'Number: n' inside an H1 tag only.
-    """
-    return render_template('5-number.html', number=n)
+def number_template_n(n):
+    """displays an HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
