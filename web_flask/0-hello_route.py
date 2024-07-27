490ab2af77e2c86ba99a3b08d5539d2814f3624f
Elpira <elpira@elpira.168.1.36>
2024-07-27 12:16:31 -0400

    web_flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application that displays 'Hello HBNB!' on the root route.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!' on the root route.
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
