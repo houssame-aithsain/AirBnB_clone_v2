f0e6ec5bdeb4df04ec55283a433342183d3aa9e1
Elpira <elpira@elpira.168.1.36>
2024-07-27 14:09:53 -0400

    web_flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,6 +45,11 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    class_name = val['__class__']
+                    self.all()[key] = classes[class_name](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Call reload() method for deserializing the JSON file to objects"""
+        self.reload()
