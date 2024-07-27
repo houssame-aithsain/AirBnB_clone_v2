3cefd2f5b215e4edb53f0cb93068b15d05120ed7
Elpira <elpira@elpira.168.1.36>
2024-07-27 13:37:36 -0400

    web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,9 +38,9 @@ def number_n(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template_n(number):
+def number_template_n(n):
     """displays an HTML page only if n is an integer"""
-    return render_template('5-number.html', number=number)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
